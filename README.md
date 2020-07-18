ca45b41b01d27693b6580de7d97b79be380ebf40
Daniel Grant <danielrgrant95@gmail.com>
2020-07-19 01:10:13 +0200

    add functionality to start, stop and list ec2 instances with boto3

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # snapshotalyzer-30000
+
+
+## Configuring
+
+shotty uses configuration created by the AWS cli. e.g.
+
+`aws configure --profile shotty`
+
+
+## Running
+
+from ./code:
+`pipenv run shotty.py <command><--project=PROJECT>`
+
+*command* is list, start or stop
+*project* is optional
--- code/shotty.py
+++ code/shotty.py
@@ -1,19 +1,75 @@
 import boto3
 import click
 
+def FilterInstances(project):
+	if project:
+		filters = [{'Name':'tag:project', 'Values':[project]}] #not clear in boto3 docs. may need to google
+		return ec2.instances.filter(Filters=filters)
+	else:
+		return ec2.instances.all()
+
 session= boto3.Session(profile_name='Shotty')
 ec2 = session.resource('ec2')
 
-@click.command()
-def ListInstances():
+@click.group()
+def instances():
+	'''Commands for instances'''
+
+
+## list instances
+@instances.command('list')
+@click.option('--project', default=None,
+	help="Only instances for project (tag project:<name>)")
+def ListInstances(project):
 	'''List ec2 instances'''
-	for i in ec2.instances.all():
+	instances= []
+	instances = FilterInstances(project)
+
+	for i in instances:
+		tags = { t['Key'] : t['Value'] for t in i.tags or [] }
+
 		print(", ".join((
 			i.id,
 			i.instance_type,
 			i.placement['AvailabilityZone'],
 			i.state['Name'],
-			i.public_dns_name)))
+			i.public_dns_name,
+			str(tags)
+			)))
+
+
+## stop instances
+@instances.command('stop')
+@click.option('--project', default=None,
+	help="Only instances for project (tag project:<name>)")
+
+def StopInstances(project):
+	'''Stop ec2 instances'''
+	instances= []
+	instances = FilterInstances(project)
+
+	for i in instances:
+		i.stop()
+
+	return
+
+
+## start instances
+@instances.command('start')
+@click.option('--project', default=None,
+	help="Only instances for project (tag project:<name>)")
+
+def StartInstances(project):
+	'''Stop ec2 instances'''
+	instances= []
+	instances = FilterInstances(project)
+
+	for i in instances:
+		i.start()
+
+	return
+
+
 
 if __name__ == "__main__":
-	ListInstances()
+	instances()
